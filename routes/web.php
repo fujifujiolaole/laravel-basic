0df635ef2210abea8270c673e465c54775a492be
fujifujiolaole <fuji.santoso@koding.sch.id>
2019-07-06 11:45:46 +0700

    Learn Routing

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,21 @@ Route::get('/', function () {
 Route::get('/user', function () {
     return "Hello word!";
 });
+Route::get('/siswa/{id}', function($id) {
+    return "Hello word!". $id;
+});
+route::get('/kariawan/{id?}', function($id=null) {
+    return "Hello kariawan".$id;
+});
+Route::Get('/siswa', function(){
+    return view("siswa");
+});
+Route::Get('/Guru', function (){
+    return View('Guru');
+});
+route::get('/admin', function(){
+    return view('admin');
+});
+route::git('/admin/{id}', function ($id) {
+    return "Hello admin".$id;
+});
